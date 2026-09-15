4b16117ecd3e21d498c257bb8a4f68412599543c
atlasdiagnosticosfitness-lab <atlasdiagnosticosfitness@gmail.com>
2026-09-15 19:35:07 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'atlas-diagnostic-fitness-v6';
+const CACHE_NAME = 'atlas-diagnostic-fitness-v7';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting())); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
